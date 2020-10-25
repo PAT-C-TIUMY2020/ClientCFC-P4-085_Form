92bac5091f4859e5e9f3681f5ee58d2ceed62824
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2020-10-25 17:29:55 +0700

    Add project files.

---
+++ ClientCFC-P4-085_Form/Form1.cs
@@ -0,0 +1,42 @@
+﻿using ServiceMtk_P1_085;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientCFC_P4_085_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+            Koordinat objk = new Koordinat();
+
+            textBox1.Text = "1";            textBox2.Text = "3";            textBox3.Text = "2";            textBox4.Text = "2";            textBox6.Text = "2";            textBox7.Text = "2";            textBox8.Text = "2";            textBox9.Text = "2";            textBox10.Text = hasilTambah.ToString();            textBox11.Text = hasilKurang.ToString();            textBox12.Text = hasilKali.ToString();            textBox13.Text = hasilBagi.ToString();
+        }
+    }
+}
